b41d81f7b41a9c46727de257713851eb80c0b93c
Andrew Carnesi <andrew.carnesi@outlook.com>
2020-09-05 12:55:33 -0500

    Fix to get calculator functional

--- RevampedCalc/Calculator.js
+++ RevampedCalc/Calculator.js
@@ -1,92 +1,73 @@
 'use strict'
-document.getElementById("button0").addEventListener("click", function(){
-  document.getElementById("input").value += "0";
-});
-document.getElementById("button1").addEventListener("click", function(){
-  document.getElementById("input").value += "1";
-});
-document.getElementById("button2").addEventListener("click", function(){
-  document.getElementById("input").value += "2";
-});
-document.getElementById("button3").addEventListener("click", function(){
-  document.getElementById("input").value += "3";
-});
-document.getElementById("button4").addEventListener("click", function(){
-  document.getElementById("input").value += "4";
-});
-document.getElementById("button5").addEventListener("click", function(){
-  document.getElementById("input").value += "5";
-});
-document.getElementById("button6").addEventListener("click", function(){
-  document.getElementById("input").value += "6";
-});
-document.getElementById("button7").addEventListener("click", function(){
-  document.getElementById("input").value += "7";
-});
-document.getElementById("button8").addEventListener("click", function(){
-  document.getElementById("input").value += "8";
-});
-document.getElementById("button9").addEventListener("click", function(){
-  document.getElementById("input").value += "9";
-});
-document.getElementById("buttonClear").addEventListener("click", function(){
-  document.getElementById("input").value = "";
-});
-document.getElementById("buttonDivide").addEventListener("click", function(){
-  document.getElementById("input").value += "/";
-});
-document.getElementById("buttonMultiply").addEventListener("click", function(){
-  document.getElementById("input").value += "*";
-});
-document.getElementById("buttonSub").addEventListener("click", function(){
-  document.getElementById("input").value += "-";
-});
-document.getElementById("buttonAdd").addEventListener("click", function(){
-  document.getElementById("input").value += "+";
-});
-document.getElementById("buttonPercent").addEventListener("click", function(){
-  document.getElementById("input").value += ")";
-});
-document.getElementById("buttonDeci").addEventListener("click", function(){
-  document.getElementById("input").value += "(";
-});
-document.getElementById("buttonSign").addEventListener("click", function(){
-  document.getElementById("input").value = "-(" + document.getElementById("input").value + ")";
-});
-document.getElementById("buttonEqual").addEventListener("click", function(){
-  Calculate(document.getElementById("input").value);
-});
+const input = document.getElementById('input');
+const buttonIds = [
+  { id: 'button0', getValue: () => '0' },
+  { id: 'button1', getValue: () => '1' },
+  { id: 'button2', getValue: () => '2' },
+  { id: 'button3', getValue: () => '3' },
+  { id: 'button4', getValue: () => '4' },
+  { id: 'button5', getValue: () => '5' },
+  { id: 'button6', getValue: () => '6' },
+  { id: 'button7', getValue: () => '7' },
+  { id: 'button8', getValue: () => '8' },
+  { id: 'button9', getValue: () => '9' },
+  { id: 'buttonClear', getValue: () => '' },
+  { id: 'buttonDivide', getValue: () => '/' },
+  { id: 'buttonMultiply', getValue: () => '*' },
+  { id: 'buttonSub', getValue: () => '-' },
+  { id: 'buttonAdd', getValue: () => '+' },
+  { id: 'buttonPercent', getValue: () => '%' },
+  { id: 'buttonDeci', getValue: () => '.' },
+  { id: 'buttonSign', getValue: (input) => {return "-(" + input.value + ")"} },
+  { id: 'buttonEqual', getValue: (input, calculation) => { calculation(input.value) } }
+];
 
+buttonIds.forEach(({ id, getValue }) => {
+  document.getElementById(id).addEventListener('click', () => {  
+    if (id == 'buttonEqual') {
+      getValue(input, Solve);
+    } 
+    else if (id == "buttonSign") {
+      input.value = getValue(input);
+    }
+    else if (id == "buttonClear") {
+      input.value = getValue();
+    }
+    else {
+      input.value += getValue();
+    }
+  })
+});
 const REGEX_ELEMENT_IDENTIFIER = /(\d+|\+|-|\*|\/|\(|\))/g;
 
 let operators = {
   "+": {
     presedence : "1",
-    operation : function(a,b){
+    operation : function(a,b) {
       return a + b;
     }
   },
   "-": {
     presedence : "1",
-    operation : function(a,b){
-      return a + b;
+    operation : function(a,b) {
+      return a - b;
     }
   },
   "*": {
     presedence : "2",
-    operation : function(a,b){
-      return a + b;
+    operation : function(a,b) {
+      return a * b;
     }
   },
   "/": {
     presedence : "2",
-    operation : function(a,b){
-      return a + b;
+    operation : function(a,b) {
+      return a / b;
     }
   },
   "(": {
     presedence : "3",
-    operation : function(a,b){
+    operation : function(a,b) {
       return a + b;
     }
   }
@@ -95,54 +76,69 @@ let operators = {
 let operatorStack = [];
 let outputQueue = [];
 
-function Calculate(expression){
+function Solve(expression) {
   let inputArray = InputToArray(expression);
   operatorStack = [];
   outputQueue = [];
   //TODO Might want to validate that there were no invalid symbols before hand that did not get captured
   ToPostfix(inputArray);
-  document.getElementById("input").value = outputQueue.join("");
+  input.value = Calculate(outputQueue);
+  console.log(operatorStack);
+  console.log(outputQueue);
 }
 
-function InputToArray(input){
+function Calculate(postFixQueue) {
+  let a, b, o;
+  while (postFixQueue.length > 1) {
+    a = Number(postFixQueue.shift());
+    b = Number(postFixQueue.shift());
+    o = postFixQueue.shift();
+
+    postFixQueue.unshift(operators[o].operation(a,b));
+    console.log(postFixQueue);
+  }
+  return postFixQueue.shift();
+}
+
+function InputToArray(input) {
   let inputStr = input;
   return inputStr.match(REGEX_ELEMENT_IDENTIFIER);
 }
 
-function ToPostfix(infixExpArray){
+function ToPostfix(infixExpArray) {
   let expression = infixExpArray;
-  for(let i = 0; i < expression.length; i++){
-    if(IsNumber(expression[i])){
+  for(let i = 0; i < expression.length; i++) {
+    if(IsNumber(expression[i])) {
       outputQueue.push(expression[i]);
     }
-    else if(IsOperator(expression[i])){
+    else if(IsOperator(expression[i])) {
       TryAddingToStack(expression[i]);
     }
-    else if(expression[i] === ")"){
+    else if(expression[i] === ")") {
       PopToOpeningParentheses();
     }
   }
   PopRemainingOperators();
 }
 
-function PopToOpeningParentheses(){
+function PopToOpeningParentheses() {
   try{
     let opFromStack = operatorStack.pop();
-    while(opFromStack !== "("){
+    while(opFromStack !== "(") {
       outputQueue.push(opFromStack);
       opFromStack = operatorStack.pop();
     }
   }
-  catch(error){
+  catch(error) {
     console.write(error);
   }
 }
 
-function IsNumber(string){
+function IsNumber(string) {
   return (string.match(/\d+/));
 }
 
-function IsOperator(string){
+function IsOperator(string) {
   return (string.match(/(\+|-|\*|\/|\()/));
 }
 
@@ -150,10 +146,10 @@ function TryAddingToStack(operator)
 {
   let presedence = GetOperatorPresedence(operator);
 
-  while(operatorStack.length > 0){
+  while(operatorStack.length > 0) {
     let opFromStack = operatorStack.pop();
 
-    if(opFromStack === "(" || presedence > GetOperatorPresedence(opFromStack)){
+    if(opFromStack === "(" || presedence > GetOperatorPresedence(opFromStack)) {
       operatorStack.push(opFromStack);
       operatorStack.push(operator);
       return;
@@ -165,10 +161,14 @@ function TryAddingToStack(operator)
   operatorStack.push(operator);
 }
 
-function PopRemainingOperators(){
-  while(operatorStack.length > 0){
+function PopRemainingOperators() {
+  while(operatorStack.length > 0) {
     outputQueue.push(operatorStack.pop());
   }
+}
+
+function GetOperatorPresedence(operator) {
+  return operators[operator].presedence;
 }
 
 //TODO Validate that the input string is valid
